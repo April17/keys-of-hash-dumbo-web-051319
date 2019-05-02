fe5afa263dabfa5e8ba072c12dab98bef19daf2c
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-02 16:39:45 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,13 @@
 require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     ary = []
     hashHolder = self
-    hashHolder.collect do |key, value|
-      if value == arguments
-        ary << key
+    arguments.each do |argument|
+      hashHolder.collect do |key, value|
+        if value == argument
+          ary << key
+        end
       end
     end
     return ary
